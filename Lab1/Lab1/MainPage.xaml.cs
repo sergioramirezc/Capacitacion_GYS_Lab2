fd1db9337f761c2bea128ffae7b27c5478156e89
Sergio Ramirez <sergioramirez@outlook.com>
2019-04-16 17:52:10 -0500

    lab2

---
+++ Lab1/Lab1/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Lab1.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Lab1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            BindingContext = new MainViewModel();
+            InitializeComponent();
+        }
+
+        private void SwitchTheme_Toggled(object sender, ToggledEventArgs e)
+        {
+            if (e.Value)
+            {
+                App.Current.Resources["PrimaryColor"] = "#550088";
+                App.Current.Resources["SecondaryColor"] = "#660022";
+            }
+            else
+            {
+                App.Current.Resources["PrimaryColor"] = "#ffffff";
+                App.Current.Resources["SecondaryColor"] = "#0366d6";
+            }
+        }
+    }
+}
---
+++ Lab1/Lab1/ViewModels/MainViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Lab1.ViewModels
+{
+    public class MainViewModel : BindableObject
+    {
+        public ICommand TapCommand { get; set; }
+
+        private string _calcValue;
+
+        public string CalcValue
+        {
+            get { return _calcValue; }
+            set
+            {
+                _calcValue = value;
+                OnPropertyChanged("CalcValue");
+            }
+        }
+
+        public MainViewModel()
+        {
+            TapCommand = new Command<string>((value) => OnTap(value));
+        }
+
+        private void OnTap(string value)
+        {
+            CalcValue += value;
+        }
+
+    }
+}
